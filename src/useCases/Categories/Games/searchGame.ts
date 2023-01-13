e12b5005f316a609658c82ee7461d214a0d233aa
Artur Watanabe <arturwatanabe@gmail.com>
2023-01-13 20:32:00 -0300

    Ajusted: Type player:Player on searchGame

--- src/useCases/Categories/Games/searchGame.ts
+++ src/useCases/Categories/Games/searchGame.ts
@@ -1,4 +1,6 @@
-export function searchGame(player: any, gameName: any) {
+import { Player } from "../../../Entities/Player/Player";
+
+export function searchGame(player: Player, gameName: any) {
   const game = player.games.findIndex(
     (g: any) => g.name.toLowerCase() == gameName.name.toLowerCase()
   );
