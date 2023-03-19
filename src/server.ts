7842054a8f279749fdbfbe29ffb5cbaada6cac19
Artur Watanabe <arturwatanabe@gmail.com>
2023-03-19 07:09:41 -0300

    fix: swagger css

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import "reflect-metadata";
 import { playerRouter } from "./routes/player.routes";
 import { gamesRouter } from "./routes/games.routes";
 import express from "express";
-import { ConnectToPostgreS } from "./Repository/postgreSQL/connect/connectToPostgreSQL.database";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
+import { css } from "./swaggerCSS";
 
 const app = express();
 const cors = require("cors");
@@ -16,10 +16,14 @@ app.use(express.json());
 
 const port = 8080;
 
+const options = {
+  customCss: css,
+};
+
 // const connectPostgreSQL = new ConnectToPostgreS();
 // connectPostgreSQL.connect();
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 app.use(playerRouter);
 app.use(gamesRouter);
 
