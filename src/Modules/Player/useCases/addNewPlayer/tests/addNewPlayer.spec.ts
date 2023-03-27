86336924ea3aabe8e1c3323237b62cad4caf205e
Artur Watanabe <arturwatanabe@gmail.com>
2023-03-27 03:05:59 -0300

    add: add already exists player test

--- src/Modules/Player/useCases/addNewPlayer/tests/addNewPlayer.spec.ts
+++ src/Modules/Player/useCases/addNewPlayer/tests/addNewPlayer.spec.ts
@@ -60,4 +60,36 @@ describe("Add new Player", () => {
 
     expect(createdPlayer).toHaveProperty("games");
   });
+
+  test("Should not be able to add a new player if email is already registered", () => {
+    expect(async () => {
+      const playerRepository = PlayersRepositoryMemory.getInstance();
+      const gameRepository = GameRepositoryMemory.getInstance();
+      const playerGamesRepository = PlayerGamesMemory.getInstance();
+
+      const playerMock: PlayerRequestType = {
+        name: "PLAYER_NAME",
+        age: 25,
+        email: "user@example.com",
+        password: "PLAYER_PASSWORD",
+      };
+
+      const addNewPlayerUseCase = new AddNewPlayerUseCase(
+        playerRepository,
+        gameRepository,
+        playerGamesRepository
+      );
+
+      await addNewPlayerUseCase.execute(playerMock);
+
+      const playerMockWithSameEmail: PlayerRequestType = {
+        name: "PLAYER_NAME1",
+        age: 25,
+        email: "user@example.com",
+        password: "PLAYER_PASSWORD2",
+      };
+
+      await addNewPlayerUseCase.execute(playerMockWithSameEmail);
+    }).rejects.toThrow("Player user@example.com already exists");
+  });
 });
